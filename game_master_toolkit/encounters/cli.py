b395eb125d8feefed88ce61d420afdf7c0e194e7
Anna Dunster <anna@annardunster.com>
2023-03-25 18:35:11 -0700

    Begin roughing in encounter code

---
+++ game_master_toolkit/encounters/cli.py
@@ -0,0 +1,13 @@
+def enc_interface():
+    print("***ENCOUNTERS***")
+    # TODO: Make some way for there to be a) tables and b) non default tables.
+    print("Using default encounter tables.")
+    print(
+        "Press Enter to generate a random encounter from all loaded tables, [tables] to see available tables, "
+        "a table number to inspect a table, or [help] for more information."
+    )
+    #     input code goes here
+
+
+# If name = main, check for args, load interface with no args, gen runs interface for all tables, otherwise args get
+# passed as if running interface
