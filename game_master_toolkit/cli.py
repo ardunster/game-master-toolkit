775060ab45f22999392e5f597bc5c29c9535d7c3
Anna Dunster <anna@annardunster.com>
2023-03-25 17:39:45 -0700

    Change interface selection parser to return Enum instead of raw number, modify tests, move interface selector into a while loop

--- game_master_toolkit/cli.py
+++ game_master_toolkit/cli.py
@@ -1,19 +1,31 @@
 # from encounters import encounters
+from enum import Enum
+
+
+class LoadInterface(Enum):
+    ERROR = -99
+    QUIT = -1
+    ENCOUNTERS = 1
 
 
 def root_interface():
-    print("###################################")
-    print("##                               ##")
-    print("##    Game Master's Toolkit      ##")
-    print("##                               ##")
-    print("###################################")
-    print("")
-    print("Welcome to Game Master's Toolkit. Would you like to:")
-    print(" 1. Run the [E]ncounter tool")
-    print(" 2. Do [S]omething else")
-    print(" 3. [Q]uit")
-    selection = input("> ")
-    parse_selection(selection)
+    running = True
+    while running:
+        print("###################################")
+        print("##                               ##")
+        print("##    Game Master's Toolkit      ##")
+        print("##                               ##")
+        print("###################################")
+        print("")
+        print("Welcome to Game Master's Toolkit. Would you like to:")
+        print(" 1. Run the [E]ncounter tool")
+        print(" 2. Do [S]omething else")
+        print(" 3. [Q]uit")
+        raw_selection = input("> ")
+        selection = parse_selection(raw_selection)
+        if selection == -1:
+            running = False
+
     # print(encounters.sample_encounters())
 
 
@@ -24,6 +36,6 @@ def parse_selection(selection):
         or (isinstance(selection, str) and selection.lower() == "e")
         or (isinstance(selection, str) and "encounters".startswith(selection.lower()))
     ):
-        return 1
+        return LoadInterface.ENCOUNTERS
     else:
-        return -1
+        return LoadInterface.ERROR
--- test/test_cli.py
+++ test/test_cli.py
@@ -2,22 +2,22 @@ from game_master_toolkit import cli
 
 
 def test_parse_selection_encounters():
-    assert cli.parse_selection("1") == 1
-    assert cli.parse_selection(1) == 1
-    assert cli.parse_selection("E") == 1
-    assert cli.parse_selection("e") == 1
-    assert cli.parse_selection("en") == 1
-    assert cli.parse_selection("enc") == 1
-    assert cli.parse_selection("encount") == 1
-    assert cli.parse_selection("encounter") == 1
-    assert cli.parse_selection("encounters") == 1
+    assert cli.parse_selection("1") == cli.LoadInterface.ENCOUNTERS
+    assert cli.parse_selection(1) == cli.LoadInterface.ENCOUNTERS
+    assert cli.parse_selection("E") == cli.LoadInterface.ENCOUNTERS
+    assert cli.parse_selection("e") == cli.LoadInterface.ENCOUNTERS
+    assert cli.parse_selection("en") == cli.LoadInterface.ENCOUNTERS
+    assert cli.parse_selection("enc") == cli.LoadInterface.ENCOUNTERS
+    assert cli.parse_selection("encount") == cli.LoadInterface.ENCOUNTERS
+    assert cli.parse_selection("encounter") == cli.LoadInterface.ENCOUNTERS
+    assert cli.parse_selection("encounters") == cli.LoadInterface.ENCOUNTERS
 
 
 def test_parse_selection_no_match():
-    assert cli.parse_selection("steve") == -1
-    assert cli.parse_selection("potato") == -1
-    assert cli.parse_selection("no words here") == -1
-    assert cli.parse_selection("qwit") == -1
-    assert cli.parse_selection("salido") == -1
-    assert cli.parse_selection(17) == -1
-    assert cli.parse_selection("nyanyanyanya") == -1
+    assert cli.parse_selection("steve") == cli.LoadInterface.ERROR
+    assert cli.parse_selection("potato") == cli.LoadInterface.ERROR
+    assert cli.parse_selection("no words here") == cli.LoadInterface.ERROR
+    assert cli.parse_selection("qwit") == cli.LoadInterface.ERROR
+    assert cli.parse_selection("salido") == cli.LoadInterface.ERROR
+    assert cli.parse_selection(17) == cli.LoadInterface.ERROR
+    assert cli.parse_selection("nyanyanyanya") == cli.LoadInterface.ERROR
