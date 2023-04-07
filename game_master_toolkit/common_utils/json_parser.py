4069e30d29802ab3e1e90bb71d0cc9d19f64191c
Anna Dunster <anna@annardunster.com>
2023-04-07 14:51:27 -0700

    Begin implementation of JsonParser class and test suite

--- game_master_toolkit/common_utils/json_parser.py
+++ game_master_toolkit/common_utils/json_parser.py
@@ -1,2 +1,37 @@
-def validate_schema(file_path, schema_name):
-    return False
+import json
+from pathlib import Path
+
+from jsonschema import validate
+
+
+class JsonParser:
+    def __init__(self, tool, dataset, filesystem: Path):
+        self.tool = tool
+        self.dataset = dataset
+        self.filesystem = filesystem
+
+    def read_files(self, filenames: list[str]):
+        data = {}
+        schema_filename = f"{self.tool}.schema.json"
+        schema_path = (
+            self.filesystem
+            / "game_master_toolkit"
+            / "common_utils"
+            / f"schemas/{schema_filename}"
+        )
+        with schema_path.open() as f:
+            schema = json.load(f)
+        for filename in filenames:
+            file_path = (
+                self.filesystem
+                / "game_master_toolkit"
+                / self.tool
+                / "data"
+                / self.dataset
+                / f"{filename}.json"
+            )
+            with file_path.open() as f:
+                file_data = json.load(f)
+                validate(instance=file_data, schema=schema)
+                data[filename] = file_data
+        return data
--- test/common_utils/test_json_parser.py
+++ test/common_utils/test_json_parser.py
@@ -1,9 +1,112 @@
-from game_master_toolkit.common_utils import json_parser
+import json
+import shutil
+from pathlib import Path
+
+import pytest
+
+from game_master_toolkit.common_utils.json_parser import JsonParser
 
 
-def test_validate_schema_encounter_valid():
-    assert json_parser.validate_schema("filename", "encounter")
+class TestJsonParser:
+    @pytest.fixture
+    def mock_file_system(self, tmp_path):
+        gmtk_dir = tmp_path / "game_master_toolkit"
+        gmtk_dir.mkdir(parents=True)
+        data_dir = gmtk_dir / "encounters" / "data" / "default"
+        data_dir.mkdir(parents=True, exist_ok=True)
+        schema_dir = gmtk_dir / "common_utils" / "schemas"
+        schema_dir.mkdir(parents=True, exist_ok=True)
+        (data_dir / "dungeon.json").write_text(
+            json.dumps(
+                [
+                    {"name": "goblin", "frequency": "common", "quantity": "2d4"},
+                    {"name": "roper", "frequency": "rare", "quantity": "1"},
+                ]
+            )
+        )
+        (data_dir / "forest.json").write_text(
+            json.dumps(
+                [
+                    {"name": "gnoll", "frequency": "uncommon", "quantity": "2d6"},
+                    {"name": "centaur", "frequency": "uncommon", "quantity": "2d6"},
+                ]
+            )
+        )
+        (data_dir / "fail1.json").write_text(
+            json.dumps(
+                [
+                    {"steve": "gnoll", "frequency": "uncommon", "quantity": "2d6"},
+                    {"name": "centaur", "frequency": "uncommon", "quantity": "2d6"},
+                ]
+            )
+        )
+        (data_dir / "fail2.json").write_text(
+            json.dumps(
+                {"name": "centaur", "frequency": "uncommon", "quantity": "2d6"},
+            )
+        )
+        actual_path_of_encounters_schema = Path(
+            "./game_master_toolkit/common_utils/schemas/encounters.schema.json"
+        )
+        shutil.copy(actual_path_of_encounters_schema, schema_dir)
+        yield tmp_path
 
+    def test_json_parser(self, mock_file_system):
+        biome = "dungeon"
+        parser = JsonParser("encounters", "default", mock_file_system)
+        data = parser.read_files([biome])
+        assert len(data) == 1
+        assert data[biome] == [
+            {"name": "goblin", "frequency": "common", "quantity": "2d4"},
+            {"name": "roper", "frequency": "rare", "quantity": "1"},
+        ]
+        biome = "forest"
+        data = parser.read_files([biome])
+        assert len(data) == 1
+        assert data[biome] == [
+            {"name": "gnoll", "frequency": "uncommon", "quantity": "2d6"},
+            {"name": "centaur", "frequency": "uncommon", "quantity": "2d6"},
+        ]
 
-def test_validate_schema_encounter_invalid():
-    assert not json_parser.validate_schema("bad_filename", "encounter")
+    # TODO: write this test correctly for expected validation failure output
+    @pytest.mark.xfail(reason="Incorrect test implementation, TODO")
+    def test_json_parser_schema_failure(self, mock_file_system):
+        parser = JsonParser("encounters", "default", mock_file_system)
+        biome = "fail1"
+        data = parser.read_files([biome])
+        assert len(data) == 1
+        assert data[biome] == [
+            {"name": "gnoll", "frequency": "uncommon", "quantity": "2d6"},
+            {"name": "centaur", "frequency": "uncommon", "quantity": "2d6"},
+        ]
+        biome = "fail2"
+        data = parser.read_files([biome])
+        assert len(data) == 1
+        assert data[biome] == [
+            {"name": "gnoll", "frequency": "uncommon", "quantity": "2d6"},
+            {"name": "centaur", "frequency": "uncommon", "quantity": "2d6"},
+        ]
+
+    # TODO: write this test correctly for expected validation failure output
+    @pytest.mark.xfail(reason="Incorrect test implementation, TODO")
+    def test_read_files_with_invalid_tool(mock_file_system):
+        parser = JsonParser("invalid_tool", "default", mock_file_system)
+        with pytest.raises(FileNotFoundError):
+            parser.read_files(["dungeon"])
+
+    # TODO: write this test correctly for expected validation failure output
+    @pytest.mark.xfail(reason="Incorrect test implementation, TODO")
+    def test_read_files_with_invalid_dataset(mock_file_system):
+        parser = JsonParser("encounters", "invalid_dataset", mock_file_system)
+        with pytest.raises(FileNotFoundError):
+            parser.read_files(["dungeon"])
+
+    # TODO: write this test correctly for expected  failure output
+    @pytest.mark.xfail(reason="Incorrect test implementation, TODO")
+    def test_read_files_with_invalid_filename(mock_file_system):
+        parser = JsonParser("encounters", "default", mock_file_system)
+        with pytest.raises(FileNotFoundError):
+            parser.read_files(["invalid_filename"])
+
+    def test_json_parser_constructor_params(self):
+        pass
