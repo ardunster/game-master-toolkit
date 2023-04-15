4faca2373eb2bab6d48431d0c6307d42259bd800
Anna Dunster <anna@annardunster.com>
2023-04-15 10:38:15 -0700

    Finish implementing split_dice

--- game_master_toolkit/common_utils/dice.py
+++ game_master_toolkit/common_utils/dice.py
@@ -1,3 +1,4 @@
+import re
 from dataclasses import dataclass
 
 from game_master_toolkit.common_utils.regex_constants import dice_notation
@@ -7,11 +8,21 @@ def split_dice(dice: str):
     dice = dice.lower()
     assert dice_notation.match(dice) is not None
     split_d = dice.split("d")
-    if len(split_d) > 1:
-        quantity = int(split_d[1])
+    if len(split_d) == 1:
+        quantity = int(split_d[0])
+        die = 1
+        modifier = 0
     else:
-        quantity = 1
-    return Dice(int(split_d[0]), quantity, 0)
+        split_plus = re.split(r"[+-]", split_d[1])
+        die = int(split_plus[0])
+        quantity = int(split_d[0])
+        if len(split_plus) == 1:
+            modifier = 0
+        else:
+            modifier = int(split_plus[1])
+            if "-" in split_d[1]:
+                modifier = -modifier
+    return Dice(quantity, die, modifier)
 
 
 @dataclass
